f588c428f8f02dac09bba0a978e43dba5b592623
Marlon.C <USTR025024@ugb.edu.sv>
2025-10-01 07:39:58 -0600

    Feat: idioma

--- js/idioma.js
+++ js/idioma.js
@@ -1,18 +1,27 @@
 
 const btnIdioma = document.getElementById('idioma')
 const textIdioma = document.getElementById('textIdioma')
+
+function comprobacionLocal(){
+    if (localStorage.getItem("proyecIndex") == null) {
+        localStorage.setItem("proyecIndex", "0");
+       
+    }    
+    if (localStorage.getItem("idioma") == null) {
+        localStorage.setItem("idioma", "ES");
+    }    
+}
+comprobacionLocal()
 btnIdioma.addEventListener("click", event =>{
     event.preventDefault()
-if (!(typeof window.localStorage !== 'undefined')) {
-  localStorage.setItem("idioma", "ES");
-}
+    comprobacionLocal()
     const idioma = localStorage.getItem("idioma");
-    console.log(idioma)
      cambio(idioma)
      
 })
 
 function cambio(idioma){
+   
     if(idioma == "EN"){
         textIdioma.innerText = "ES"
         localStorage.setItem("idioma", "ES");
@@ -24,6 +33,8 @@ function cambio(idioma){
     inicioCambio(idioma)
     menuCambio(idioma)
    sobreMiCambio(idioma)
+   proyectosMenuCambio(idioma)
+   proyectosCambio(idioma)
 }
 
 //datos y funcion de inicio
@@ -113,6 +124,9 @@ const sobreMiDatos = [
         parrafo: 'I have skills in both front-end and back-end development, eager to provide creative and functional solutions, and highly adaptable to different collaborative environments. I have knowledge of C#, HTML, CSS, JS - JavaScript, and SQL databases.'
     }
 ]
+
+
+
 function sobreMiCambio(idioma){
     if(idioma == "EN"){
         SobreMiTitulo.innerText = sobreMiDatos[0].titulo
@@ -125,3 +139,174 @@ function sobreMiCambio(idioma){
     }
 }
 
+//datos y funciones de cambio de idioma del menu del proyecto
+
+const proyectosTitulo = document.getElementById("proyectosTitle");
+const proyectosMenu1 = document.getElementById("btnsoftware");
+const proyectosMenu2 = document.getElementById("btnSitioWeb");
+const proyectosMenu3 = document.getElementById("btnAppMovil");
+const proyectosMenu4 = document.getElementById("btnAppWeb");
+
+
+const proyecMenuDatos = [
+    {
+        titulo: "Proyectos",
+        menu1: "Software",
+        menu2: "Sitio Web",
+        menu3: "Aplicación movil",
+        menu4: "Aplicación Web",
+    },
+    {
+        titulo: "Projects",
+        menu1: "Software",
+        menu2: "Website",
+        menu3: "Mobile App",
+        menu4: "Web App",
+    }
+]
+function proyectosMenuCambio(idioma){
+    if(idioma == "EN"){
+        proyectosTitulo.innerText = proyecMenuDatos[0].titulo
+        proyectosMenu1.innerText = proyecMenuDatos[0].menu1
+        proyectosMenu2.innerText = proyecMenuDatos[0].menu2
+        proyectosMenu3.innerText = proyecMenuDatos[0].menu3
+        proyectosMenu4.innerText = proyecMenuDatos[0].menu4
+    }else{
+        proyectosTitulo.innerText = proyecMenuDatos[1].titulo
+        proyectosMenu1.innerText = proyecMenuDatos[1].menu1
+        proyectosMenu2.innerText = proyecMenuDatos[1].menu2
+        proyectosMenu3.innerText = proyecMenuDatos[1].menu3
+        proyectosMenu4.innerText = proyecMenuDatos[1].menu4
+    }
+}
+
+
+
+//datos y funciones de cambio de idioma de descripcion del proyecto
+
+const proyectTitulo = document.getElementById("proyectTitulo");
+const proyectImg = document.getElementById("proyectImg");
+const proyectDescripcion = document.getElementById("proyectDescripcion");
+const proyectoTecno = document.getElementById("proyectoTecno");
+const proyectoRol = document.getElementById("proyectoRol");
+const proyectTecnoList = document.getElementById("proyectTecnoList");
+const ProyectlickGithub = document.getElementById("ProyectlickGithub");
+
+
+const proyectDescripcionDatos = {
+        "0":{
+           ES: {
+            titulo: "Farmacia Divino Niño - Software",
+            imagen: 'img/imgProyect/software.png',
+            descripcion: "Sistema de gestión de inventario por lote, ventas y devoluciones de medicamentos.",
+            tecnologias: "Tecnologias utilizadas",
+            proyectoRol: "Rol desempeñado: desarrollador Frond-end y Back-end",
+            tecnoList: 
+            '<li>c#</li><li>mySQL</li><li>Vs Studio 2022</li><li>.NET</li><li>HTML</li><li>css</li>',
+            linkGithub: "https://github.com/marlon-cruz/proyecto",
+            },
+           EN: {
+            titulo: "Farmacia Divino Niño - Software",
+            imagen: "img/imgProyect/software.png",
+            descripcion: "Batch inventory management system, sales and returns of medications.",
+            proyectoRol: "Role played: Front-end and Back-end developer",
+            tecnologias: "Technologies used",
+            tecnoList: 
+            '<li>c#</li><li>mySQL</li><li>Vs Studio 2022</li><li>.NET</li><li>HTML</li><li>css</li>',
+            linkGithub: "https://github.com/marlon-cruz/proyecto",
+            }
+        },
+        "1":{
+           ES: {
+            titulo: "Farmacia Divino Niño - Sitio Web",
+            imagen: 'img/imgProyect/software.png',
+            descripcion: "Sistema de gestión de inventario por lote, ventas y devoluciones de medicamentos.",
+            tecnologias: "Tecnologias utilizadas",
+            proyectoRol: "Rol desempeñado: desarrollador Frond-end y Back-end",
+            tecnoList: 
+            '<li>c#</li><li>mySQL</li><li>Vs Studio 2022</li><li>.NET</li><li>HTML</li><li>css</li>',
+            linkGithub: "https://github.com/marlon-cruz/proyecto",
+            },
+           EN: {
+            titulo: "Farmacia Divino Niño - Software",
+            imagen: "img/imgProyect/software.png",
+            descripcion: "Batch inventory management system, sales and returns of medications.",
+            proyectoRol: "Role played: Front-end and Back-end developer",
+            tecnologias: "Technologies used",
+            tecnoList: 
+            '<li>c#</li><li>mySQL</li><li>Vs Studio 2022</li><li>.NET</li><li>HTML</li><li>css</li>',
+            linkGithub: "https://github.com/marlon-cruz/proyecto",
+            }
+        },
+        "2":{
+           ES: {
+            titulo: "PetCare - Aplicación movil",
+            imagen: 'img/imgProyect/software.png',
+            descripcion: "Sistema de gestión de inventario por lote, ventas y devoluciones de medicamentos.",
+            tecnologias: "Tecnologias utilizadas",
+            proyectoRol: "Rol desempeñado: desarrollador Frond-end y Back-end",
+            tecnoList: 
+            '<li>c#</li><li>mySQL</li><li>Vs Studio 2022</li><li>.NET</li><li>HTML</li><li>css</li>',
+            linkGithub: "https://github.com/marlon-cruz/proyecto",
+            },
+           EN: {
+            titulo: "Farmacia Divino Niño - Software",
+            imagen: "img/imgProyect/software.png",
+            descripcion: "Batch inventory management system, sales and returns of medications.",
+            proyectoRol: "Role played: Front-end and Back-end developer",
+            tecnologias: "Technologies used",
+            tecnoList: 
+            '<li>c#</li><li>mySQL</li><li>Vs Studio 2022</li><li>.NET</li><li>HTML</li><li>css</li>',
+            linkGithub: "https://github.com/marlon-cruz/proyecto",
+            }
+        },
+        "3":{
+           ES: {
+            titulo: "Red de esperanza - Aplicación Web",
+            imagen: 'img/imgProyect/software.png',
+            descripcion: "Sistema de gestión de inventario por lote, ventas y devoluciones de medicamentos.",
+            tecnologias: "Tecnologias utilizadas",
+            proyectoRol: "Rol desempeñado: desarrollador Frond-end y Back-end",
+            tecnoList: 
+            '<li>c#</li><li>mySQL</li><li>Vs Studio 2022</li><li>.NET</li><li>HTML</li><li>css</li>',
+            linkGithub: "https://github.com/marlon-cruz/proyecto",
+            },
+           EN: {
+            titulo: "Farmacia Divino Niño - Software",
+            imagen: "img/imgProyect/software.png",
+            descripcion: "Batch inventory management system, sales and returns of medications.",
+            proyectoRol: "Role played: Front-end and Back-end developer",
+            tecnologias: "Technologies used",
+            tecnoList: 
+            '<li>c#</li><li>mySQL</li><li>Vs Studio 2022</li><li>.NET</li><li>HTML</li><li>css</li>',
+            linkGithub: "https://github.com/marlon-cruz/proyecto",
+            }
+        }
+    }
+ 
+function proyectosCambio(idioma){
+    console.log(idioma)
+    const indiceProyect = localStorage.getItem("proyecIndex")
+    if(idioma = "-1"){
+        idioma = localStorage.getItem("idioma")
+    }
+    console.log(idioma)
+    
+    if(idioma == "ES"){
+        proyectTitulo.innerText = proyectDescripcionDatos[indiceProyect].ES.titulo
+        proyectImg.src = proyectDescripcionDatos[indiceProyect].ES.imagen
+        proyectDescripcion.innerText = proyectDescripcionDatos[indiceProyect].ES.descripcion
+        proyectoTecno.innerText = proyectDescripcionDatos[indiceProyect].ES.tecnologias
+        proyectoRol.innerText = proyectDescripcionDatos[indiceProyect].ES.proyectoRol
+        proyectTecnoList.innerHTML = proyectDescripcionDatos[indiceProyect].ES.tecnoList
+        ProyectlickGithub.href = proyectDescripcionDatos[indiceProyect].ES.linkGithub
+    }else{
+        proyectTitulo.innerText = proyectDescripcionDatos[indiceProyect].EN.titulo
+        proyectImg.src = proyectDescripcionDatos[indiceProyect].EN.imagen
+        proyectDescripcion.innerText = proyectDescripcionDatos[indiceProyect].EN.descripcion
+        proyectoRol.innerText = proyectDescripcionDatos[indiceProyect].EN.proyectoRol
+        proyectoTecno.innerText = proyectDescripcionDatos[indiceProyect].EN.tecnologias
+        proyectTecnoList.innerHTML = proyectDescripcionDatos[indiceProyect].EN.tecnoList
+        ProyectlickGithub.href = proyectDescripcionDatos[indiceProyect].EN.linkGithub
+    }
+}
